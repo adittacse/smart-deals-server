3d39f6fd01ff9f9d1f1ff2e34daf4f45d7ff1d55
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-01 02:33:20 +0600

    bids api created

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-const uri =
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gkaujxr.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gkaujxr.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,9 +32,18 @@ async function run() {
 
         const db = client.db("smart_DB");
         const productsCollection = db.collection("products");
+        const bidsCollection = db.collection("bids");
 
+        // products collections api's
         app.get("/products", async (req, res) => {
-            const cursor = productsCollection.find();
+            // const projectFields = { title: 1 };
+            // const cursor = productsCollection.find().sort({ price_min: -1 }).limit(5).project(projectFields);
+            const email = req.query.email;
+            const query = {};
+            if (email) {
+                query.email = email;
+            }
+            const cursor = productsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         });
@@ -60,11 +68,15 @@ async function run() {
             const update = {
                 $set: {
                     name: updatedProduct.name,
-                    price: updatedProduct.price
-                }
+                    price: updatedProduct.price,
+                },
             };
             const options = {};
-            const result = await productsCollection.updateOne(query, update, options);
+            const result = await productsCollection.updateOne(
+                query,
+                update,
+                options,
+            );
             res.send(result);
         });
 
@@ -72,6 +84,38 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // bids collections api's
+        app.get("/bids", async (req, res) => {
+            const email = req.query.email;
+            const query = {};
+            if (email) {
+                query.buyer_email = email;
+            }
+            const cursor = bidsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get("/bids/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = bidsCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.post("/bids", async (req, res) => {
+            const newBid = req.body;
+            const result = await bidsCollection.insertOne(newBid);
+            res.send(result);
+        });
+
+        app.delete("/bids/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bidsCollection.deleteOne(query);
             res.send(result);
         });
 
